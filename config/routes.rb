b85133200cee0d972baaa1f9895f8272eefac1e1
Jeffrey Chang <me@jeffreychang.com>
2012-04-27 02:36:18 -0700

    Finish demo app yay! :D

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 FirstApp::Application.routes.draw do
+  resources :microposts
+
+  resources :items
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
